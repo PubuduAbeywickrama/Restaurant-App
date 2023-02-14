39788106e9c3086528336fd0cce79c0c42e4bfcd
PubuduAbeywickrama <pubudutharaka6@gmail.com>
2023-02-14 22:25:37 +0530

    changes

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants'; 
 import './Header.css';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+export default function () {
+  return (
+    <div className='app__header app__wrapper section__padding' id="home">
+          <div className="app__wrapper_info">
+              <SubHeading title="Chase the new flavour" />
+              <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+              <p className="p__opensans" style={{margin: '2rem 0'}}>this is a paragraph under the heading</p>
+              <button className="custom__button">Explore Menu</button>
+          </div>
+          <div className="app__wrapper_img">
+              <img src={images.welcome} alt="header_img" />
+          </div>
+    </div>
+ 
+  )
+}
 
-export default Header;
